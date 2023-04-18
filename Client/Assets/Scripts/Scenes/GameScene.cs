b424b85be02e153ae9d42098cab6153619190962
YeongJ <juyeong1996@gmail.com>
2023-04-18 21:58:41 +0900

    [Client] 유니티 프로젝트 커밋

---
+++ Client/Assets/Scripts/Scenes/GameScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : BaseScene
+{
+    protected override void Init()
+    {
+        base.Init();
+
+        SceneType = Define.Scene.Game;
+
+        Managers.Map.LoadMap(1);
+
+        Screen.SetResolution(640, 480, false);
+    }
+
+    public override void Clear()
+    {
+        
+    }
+}
