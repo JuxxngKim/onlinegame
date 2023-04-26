68f361dbac6b3a4ecad3fc2e6aab8e4322f2817d
YeongJ <juyeong1996@gmail.com>
2023-04-26 23:40:43 +0900

    [Server] GameMap 작업중

--- Server/GameServer/GameMap.cpp
+++ Server/GameServer/GameMap.cpp
@@ -1,11 +1,31 @@
 #include "pch.h"
 #include "GameMap.h"
 
+Vector2Int Vector2Int::up{ 0, 1 };
+Vector2Int Vector2Int::down{ 0, -1 };
+Vector2Int Vector2Int::left{ -1, 0 };
+Vector2Int Vector2Int::right{ 1, 0 };
+
 GameMap::GameMap(int32 mapId)
 {
 	LoadMap(mapId);
 }
 
+bool GameMap::CanGo(Vector2Int cellPos, bool checkObjects = true)
+{
+	if (cellPos.X < _minX || cellPos.X > _maxX)
+		return false;
+	if (cellPos.Y < _minY || cellPos.Y > _maxY)
+		return false;
+
+	int x = cellPos.X - _minY;
+	int y = _maxX - cellPos.Y;
+	//return !_collision[y][x] && (!checkObjects || _objects[y][x] == nullptr);
+	return false;
+}
+
+
 void GameMap::LoadMap(int mapId)
 {
+	
 }
--- Server/GameServer/GameMap.h
+++ Server/GameServer/GameMap.h
@@ -1,13 +1,56 @@
 #pragma once
+
+struct Pos
+{
+public:
+	Pos(int32 y, int32 x) { Y = y; X = x; }
+
+public:
+	int32 Y;
+	int32 X;
+};
+
+struct Vector2Int
+{
+public:
+	Vector2Int(int32 x, int32 y) { Y = y; X = x; }
+
+	static Vector2Int up;
+	static Vector2Int down;
+	static Vector2Int left;
+	static Vector2Int right;
+
+public:
+	friend Vector2Int operator+(const Vector2Int& a, const Vector2Int& b)
+	{
+		return Vector2Int(a.X + b.X, a.Y + b.Y);
+	}
+
+	friend Vector2Int operator-(const Vector2Int& a, const Vector2Int& b)
+	{
+		return Vector2Int(a.X - b.X, a.Y - b.Y);
+	}
+
+public:
+	int32 Y;
+	int32 X;
+};
+
 class GameMap
 {
 public:
 	GameMap(int32 mapId);
 
+public:
+	bool CanGo(Vector2Int cellPos, bool checkObjects = true);
+
 private:
 	void LoadMap(int mapId);
 
 private:
+	/*bool** _collision;
+	GameObject* _objects;*/
+
 	int _minX;
 	int _minY;
 	int _maxX;
