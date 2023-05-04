e61b64f0143963182376c2596d4fac479cb13a34
JuyeongKim <juyeong1996@gravity.co.kr>
2023-05-04 11:52:57 +0900

    [Server] Player Stat

--- Server/GameServer/Player.cpp
+++ Server/GameServer/Player.cpp
@@ -5,4 +5,11 @@ Player::Player(uint64 Id, Protocol::GameObjectType objectType, weak_ptr<class Ro
     : GameObject(Id, objectType, room)
 {
     _objectInfo.set_objecttype(Protocol::GameObjectType::PLAYER);
+
+    _statInfo.set_level(1);
+    _statInfo.set_attack(1);
+    _statInfo.set_hp(30);
+    _statInfo.set_maxhp(30);
+    _statInfo.set_speed(3);
+    _statInfo.set_totalexp(0);
 }
